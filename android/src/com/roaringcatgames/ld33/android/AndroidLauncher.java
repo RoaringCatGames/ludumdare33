107be5dd287c59668973b54a45f8d6e5bbd46d83
Barryrowe <barryrowe@gmail.com>
2015-08-22 00:32:04 -0400

    Initial Project Setup
    Setting up initial project. let's get going. Jam Time.

---
+++ android/src/com/roaringcatgames/ld33/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.roaringcatgames.ld33.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.roaringcatgames.ld33.MonsterDancer;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MonsterDancer(), config);
+	}
+}

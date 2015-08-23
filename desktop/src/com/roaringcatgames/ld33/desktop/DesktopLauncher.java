12e9a668959c68f6f7c6ef9d9576c555b68c639f
Barryrowe <barryrowe@gmail.com>
2015-08-23 14:59:20 -0400

    Updating html proje
    html page updated a littl.

--- desktop/src/com/roaringcatgames/ld33/desktop/DesktopLauncher.java
+++ desktop/src/com/roaringcatgames/ld33/desktop/DesktopLauncher.java
@@ -7,7 +7,7 @@ import com.roaringcatgames.ld33.MonsterDancer;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.title = "Become THE Monster";
+		config.title = "Saturday Morning Destruction";
 		config.width = 1200;
 		config.height = 800;
 		new LwjglApplication(new MonsterDancer(), config);
